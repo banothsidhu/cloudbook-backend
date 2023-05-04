1181ccce8a230f9352d786164dcbbcee09b23abb
banothsidhu <sidhubanoth207@gmail.com>
2023-05-04 09:24:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const connectToMongoDB = require('./db')
+const app = express()
+const cors = require('cors')
+// hey this 
+const port = 5000
+connectToMongoDB()
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('!')
+})
+const corsOptions = {
+  origin:"https://mycloudbook.vercel.app"
+};
+
+app.use(cors(corsOptions));
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
+app.listen(port, () => {
+  console.log(`InoteBook Backend running at http://localhost:${port}`)})
